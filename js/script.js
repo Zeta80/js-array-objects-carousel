4daeeeee2185638cc10c860300b52ea215a1a300
Mirko Robbi <mirko.robbi@gmail.com>
2022-10-22 19:26:19 +0200

    finish exercise

--- js/script.js
+++ js/script.js
@@ -16,58 +16,78 @@
 // BONUS 3: []
 // Aggiungere bottoni di start/stop e di inversione del meccanismo di autoplay. 
 
-// const images = [
-//     {
-//         image: 'img/assets/images/img/01.webp',
-//         title: 'Marvel\'s Spiderman Miles Morale',
-//         text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-//     }, {
-//         image: 'img/assets/images/img/02.webp',
-//         title: 'Ratchet & Clank: Rift Apart',
-//         text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-//     }, {
-//         image: 'img/assets/images/img/03.webp',
-//         title: 'Fortnite',
-//         text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-//     }, {
-//         image: 'img/assets/images/img/04.webp',
-//         title: 'Stray',
-//         text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-//     }, {
-//         image: 'img/assets/images/img/05.webp',
-//         title: "Marvel's Avengers",
-//         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
-//     }
-// ];
-const images = [
-    "img/assets/images/img/01.webp",
-    "img/assets/images/img/02.webp",
-    "img/assets/images/img/03.webp",
-    "img/assets/images/img/04.webp",
-    "img/assets/images/img/05.webp",
+const games = [
+    {
+        image: 'img/assets/images/img/01.webp',
+        title: 'Marvel\'s Spiderman Miles Morale',
+        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+    }, {
+        image: 'img/assets/images/img/02.webp',
+        title: 'Ratchet & Clank: Rift Apart',
+        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+    }, {
+        image: 'img/assets/images/img/03.webp',
+        title: 'Fortnite',
+        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+    }, {
+        image: 'img/assets/images/img/04.webp',
+        title: 'Stray',
+        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+    }, {
+        image: 'img/assets/images/img/05.webp',
+        title: "Marvel's Avengers",
+        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+    }
 ];
+// const images = [
+//     "img/assets/images/img/01.webp",
+//     "img/assets/images/img/02.webp",
+//     "img/assets/images/img/03.webp",
+//     "img/assets/images/img/04.webp",
+//     "img/assets/images/img/05.webp",
+// ];
 
 
-//VERIFICA TESTO OLGA
-console.log(images);
+//VERIFICA TESTO 
+console.log(games);
 const itemsContainer = document.querySelector(".items");
 const thumbsContainer = document.querySelector(".thumbs");
 let itemsList = "";
 let thumbsList = "";
-for (let i = 0; i < images.length; i++) {
-    // Creo le immagini
+
+games.forEach(game => {
     itemsList += `
-            <div class="item">
-                <img src="${images[i]}" alt="" />
-            </div>`;
+             <div class="item">
+                 <img src="${game.image}" alt="" />
+                 <h3> ${game.title} </h3>
+                 <p> ${game.text} </p>
+             </div>`;
     // Creo i thumbs
-    thumbsList += `
+    thumbsList = `
             <div class="thumb">
-                <img src="${images[i]}" alt="" />
-            </div>`;
-}
-itemsContainer.innerHTML = itemsList;
-thumbsContainer.innerHTML += thumbsList;
+                 <img src="${game.image}" alt="" />
+             </div>`;
+    console.log(thumbsList);
+    itemsContainer.innerHTML = itemsList;
+    thumbsContainer.innerHTML += thumbsList;
+
+});
+
+
+// for (let i = 0; i < images.length; i++) {
+//     // Creo le immagini
+//     itemsList += `
+//             <div class="item">
+//                 <img src="${images[i]}" alt="" />
+//             </div>`;
+//     // Creo i thumbs
+//     thumbsList += `
+//             <div class="thumb">
+//                 <img src="${images[i]}" alt="" />
+//             </div>`;
+// }
+// itemsContainer.innerHTML = itemsList;
+// thumbsContainer.innerHTML += thumbsList;
 
 // Lo stato iniziale dello slider
 const sliderItems = document.getElementsByClassName("item");
